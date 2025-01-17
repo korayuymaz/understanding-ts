a17172e3181e2407548efef1394442a7a8323e65
Koray <koray.uymaz@agesa.com.tr>
2025-01-17 20:02:54 +0300

    literal type usage

--- app.ts
+++ app.ts
@@ -1,12 +1,29 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string, // union type
+  input2: number | string, // union type
+  resultConversion: "as-number" | "as-text" // literal type
+) {
   let result: number | string;
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
   } else {
     result = input1.toString() + input2.toString();
   }
+
+  // if (resultConversion === "as-number") {
+  //   return +result;
+  // } else if (resultConversion === "as-text") {
+  //   return result.toString();
+  // }
+
   return result;
 }
-
-const combinedAges = combine(30, 26);
-const combineNames = combine("Max", "Anna");
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+const combineNames = combine("Max", "Anna", "as-text");
+console.log(combineNames);
