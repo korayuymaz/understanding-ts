b22661e135ffe66e90903f28a82172b201e8ab25
Koray <koray.uymaz@agesa.com.tr>
2025-01-17 20:17:09 +0300

    void return type

--- app.ts
+++ app.ts
@@ -1,26 +1,11 @@
-type Combinable = number | string; // type alias for union type
-type ConversionDescriptor = "as-number" | "as-text"; // type alias for literal type
-
-function combine(
-  input1: Combinable, // type alias
-  input2: Combinable, // type alias
-  resultConversion: ConversionDescriptor // type alias
-) {
-  let result: number | string;
-  if (
-    (typeof input1 === "number" && typeof input2 === "number") ||
-    resultConversion === "as-number"
-  ) {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
+function addNumbers(n1: number, n2: number) {
+  const result = n1 + n2;
   return result;
 }
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-const combineNames = combine("Max", "Anna", "as-text");
-console.log(combineNames);
+
+function printResultVoid(num: number): void {
+  // void return type means no return value
+  console.log("Result: " + num);
+}
+
+printResultVoid(addNumbers(5, 12));
