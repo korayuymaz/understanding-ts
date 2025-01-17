12715b3a28a3c16ff614d86b3b9a9347955ef864
Koray <koray.uymaz@agesa.com.tr>
2025-01-17 22:24:36 +0300

    Callback function decleration

--- app.ts
+++ app.ts
@@ -8,10 +8,19 @@ function printResultVoid(num: number): void {
   console.log("Result: " + num);
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
 printResultVoid(addNumbers(5, 12));
 
-let combineValues: (a: number, b: number) => number; // Function type 
+let combineValues: (a: number, b: number) => number; // Function type
 
 combineValues = addNumbers;
 
 console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  console.log(result); // Callback function
+});
