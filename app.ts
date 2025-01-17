1f186f23eab9cc2966d08f067dc3a612623814b4
Koray <koray.uymaz@agesa.com.tr>
2025-01-17 19:57:26 +0300

    union-types result fix

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 function combine(input1: number | string, input2: number | string) {
-  let result;
+  let result: number | string;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
