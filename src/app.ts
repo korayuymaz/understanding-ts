d4cb85b0487911ca551d9bef2cea83f8eb48fcdf
Koray <koray.uymaz@agesa.com.tr>
2025-01-22 19:14:12 +0300

    set and get methods

--- src/app.ts
+++ src/app.ts
@@ -27,8 +27,25 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
+  }
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value!");
+    }
+    this.addReport(value);
   }
 
   addEmployee(employee: string): void {
@@ -40,18 +57,10 @@ class AccountingDepartment extends Department {
 
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
 
   printReports() {
     console.log(this.reports);
   }
 }
-
-const accounting = new AccountingDepartment("d1", []);
-
-accounting.addReport("Something went wrong...");
-accounting.addReport("Something went right...");
-
-accounting.addEmployee("Max");
-accounting.addEmployee("Manu");
-accounting.printEmployeeInformation();
