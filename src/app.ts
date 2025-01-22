60c511037f3fe1fd649bdde8c99d702a1f3ddec9
Koray <koray.uymaz@agesa.com.tr>
2025-01-22 21:15:01 +0300

    type guard samples

--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,28 @@ const e1 = {
   privileges: ["create-server"],
   startDate: new Date(),
 };
+
+type Combinable = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  if (typeof a === "string" || typeof b === "string") {
+    // type guard
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Admin | GeneralEmployee;
+
+function printEmployeeInformation(emp: UnknownEmployee) { 
+  console.log("Name: " + emp.name);
+  if ("privileges" in emp) { // type guard
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) { // type guard
+    console.log("Start Date: " + emp.startDate);
+  }
+}
