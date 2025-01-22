634b4c96d601615a9ac86cb5f0c953f38579f8e9
Koray <koray.uymaz@agesa.com.tr>
2025-01-22 19:51:45 +0300

    implementing a interface with class

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
-interface Person {
+interface Greetable {
+  // interface is a blueprint for an object
+  // that defines the properties and methods that a class must implement
   name: string;
-  age: number;
-
-  greet: (phrase: string) => void;
+  greet(phrase: string): void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  // implements keyword is used to implement an interface
+  name: string;
+  age = 30; 
 
-user1 = {
-  name: "Max",
-  age: 30,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(phrase + " " + this.name);
-  },
-};
+  }
+}
