ea4645b0698aec02084f4c215a983c949937a9c2
Koray <koray.uymaz@agesa.com.tr>
2025-01-22 21:21:25 +0300

    discriminated union

--- src/app.ts
+++ src/app.ts
@@ -66,7 +66,32 @@ const v2 = new Truck();
 
 function useVehicle(vehicle: Vehicle) {
   vehicle.drive();
-  if (vehicle instanceof Truck) { // type guard for classes
+  if (vehicle instanceof Truck) {
+    // type guard for classes
     vehicle.loadCargo(1000);
   }
+}
+
+interface Bird {
+  type: "bird";
+  flyingSpeed: number;
+}
+
+interface Horse {
+  type: "horse";
+  runningSpeed: number;
+}
+
+type Animal = Bird | Horse; // discriminated union
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+      break;
+    case "horse":
+      speed = animal.runningSpeed;
+  }
+  console.log("Moving at speed: " + speed);
 }
