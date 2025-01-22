2fed82b48780c9198adefaa4736785cb5a0d330d
Koray <koray.uymaz@agesa.com.tr>
2025-01-22 20:51:08 +0300

    extending interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
-interface Greetable {
+interface Named {
   // interface is a blueprint for an object
   // that defines the properties and methods that a class must implement
-  name: string;
+  readonly name: string;
+}
+
+interface Greetable extends Named {
+  // interface is a blueprint for an object
+  // that defines the properties and methods that a class must implement
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
   // implements keyword is used to implement an interface
   name: string;
-  age = 30; 
+  age = 30;
 
   constructor(n: string) {
     this.name = n;
