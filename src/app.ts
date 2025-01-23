94e19d46ba0391e566e90f8e539a03b634c76628
Koray <koray.uymaz@agesa.com.tr>
2025-01-23 20:00:38 +0300

    Creating generics with constraints

--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,10 @@ const promise: Promise<string> = new Promise<string>((resolve, reject) => {
     resolve("This is done!");
   }, 2000);
 });
+
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj.name); // Max
